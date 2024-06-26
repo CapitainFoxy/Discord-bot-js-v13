c0da0b3e4dcc28cdc5433e2dce157752f545db2d
CapitainFoxy <czumbilkonrad2009@gmail.com>
2024-06-26 22:03:42 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+
+// Betöltjük a konfigurációs adatokat
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+
+// Létrehozzuk a botot az intents-ekkel
+const client = new Client({ 
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Intents.FLAGS.GUILD_INTEGRATIONS,
+        Intents.FLAGS.GUILD_WEBHOOKS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING
+    ]
+});
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(config.token);
